5cc8e923a8e438750a358e85b09e477598a87844
Roman-Port <darkwings220@gmail.com>
2019-08-01 16:01:35 -0500

    Added StructureDisplayMetadata_StructureType back.

--- ArkSaveEditor/ArkEntries/StructureDisplayMetadata.cs
+++ ArkSaveEditor/ArkEntries/StructureDisplayMetadata.cs
@@ -13,6 +13,7 @@ namespace ArkSaveEditor.ArkEntries
         public float captureSize; //Ortho capture size, in game meters
         public int capturePixels; //The size, in pixels, of the source image
         public float rotationOffset; //Offset to apply to the rotation
+        public StructureDisplayMetadata_StructureType type; //Type of structure
     }
 
     public enum StructureDisplayMetadata_DisplayType
@@ -20,5 +21,14 @@ namespace ArkSaveEditor.ArkEntries
         Standard = 0, //Shows this at the level according to the Z-axis
         AlwaysTop = 1, //Always shows this on top of other structures
 
+    }
+
+    public enum StructureDisplayMetadata_StructureType
+    {
+        Foundation,
+        Ceiling,
+        Wall,
+        Inventory,
+        Tool
     }
 }
